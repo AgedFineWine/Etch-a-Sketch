3be9b53a92442100a7995622d2c7d8dffa783738
evan <101075554+AgedFineWine@users.noreply.github.com>
2024-01-09 19:47:33 -0500

    implemented a working prompt feature

--- script.js
+++ script.js
@@ -1,31 +1,30 @@
-for (let num = 0; num < 16 * 16; num++) {
-   const container = document.querySelector('.container');
-    let newDiv = document.createElement('div');
-    newDiv.classList.add('square-div');
-    container.appendChild(newDiv);
-}
-
 const chooseColor = () => Math.floor((Math.random() * 255));
-const squareDiv = document.querySelectorAll('.square-div');
 
-squareDiv.forEach(reference => reference.addEventListener('mouseover', () => {
-    reference.style.background = `rgb(${chooseColor()}, ${chooseColor()}, ${chooseColor()})`;
-}));
+const createCanvas = lengthNumber => {
+    const container = document.querySelector('.container');
+    while (container.firstChild) {
+        container.removeChild(container.lastChild);
+    }
+    for (let row=0; row < lengthNumber**2; row++){
+        let newDiv = document.createElement('div');
+        newDiv.classList.add('square-div');
+        container.appendChild(newDiv);
+    }
+    container.style.setProperty('--length', lengthNumber);
+    const squareDiv = document.querySelectorAll('.square-div');
+    squareDiv.forEach(reference => reference.addEventListener('mouseover', () => {
+        reference.style.background = `rgb(${chooseColor()}, ${chooseColor()}, ${chooseColor()})`;
+    }));
+}
 
 const reset = document.querySelector("#reset");
 reset.addEventListener('click', () => {
-    const container = document.querySelector('.container');
     let lengthNumber = prompt('Enter the number of squares per side:');
     while (lengthNumber > 100) {
         lengthNumber = prompt('Enter the number of squares per side:');
     }
-    while (container.firstChild) {
-        container.removeChild(container.lastChild)
-    }
-    for (let num = 0; num < lengthNumber; num++) {
-        const container = document.querySelector('.container');
-         let newDiv = document.createElement('div');
-         newDiv.classList.add('square-div');
-         container.appendChild(newDiv);
-     }
+
+    createCanvas(lengthNumber);
 });
+
+createCanvas(16);
